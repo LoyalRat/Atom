ed41ca4a7efa4a54f457dacb21c71b7a8ab3b00d
LoyalRat <ctoxicz@gmail.com>
2021-11-01 07:00:32 +0000

    added intial setup for components

--- atom/src/App.js
+++ atom/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import { Card } from './Components'
 
 function App() {
   return (
     <div className="App">
+      <Card></Card>
       YO!!
     </div>
   );
---
+++ atom/src/Components/Card/Card.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import styled from '@emotion/styled'
+
+const StyledDiv = styled('div')`
+    background-color: black;
+`
+export const Card = ()=> {
+    return <StyledDiv>set</StyledDiv>
+}
